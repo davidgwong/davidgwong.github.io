85d8d989543b8288ceff7ef9d701b8c35d9c7b08
JustAddMayonnaise <davidw4122@gmail.com>
2016-06-02 22:25:09 -0700

    Added the scrollspy effect and now implementing slick carousel

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,4 +1,62 @@
-$('body').scrollspy({
-    target: '.bs-docs-sidebar',
-    offset: 10
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+$(".carousel").swipe( {
+    //Generic swipe handler for all directions
+    swipeLeft:function(event, direction, distance, duration, fingerCount) {
+        $(this).parent().carousel('prev'); 
+    },
+    swipeRight:function(event, direction, distance, duration, fingerCount) {
+        $(this).parent().carousel('next'); 
+    },
+    //Default is 75px, set to 0 for demo so any distance triggers swipe
+    threshold:0
 });
+
+// Source: http://jsfiddle.net/mekwall/up4nu/
+// Cache selectors
+var lastId,
+    topMenu = $("#sidebar"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 300);
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+   }                   
+});
+
