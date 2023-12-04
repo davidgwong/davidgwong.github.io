3a2391bd90a3733d1f72409964a853d791ec14a1
David Wong <11532851+davidgwong@users.noreply.github.com>
2023-12-04 15:41:54 -0800

    Revise to use HashRouter. Add 404.html.

--- davidgwong.github.io/Router.tsx
+++ davidgwong.github.io/Router.tsx
@@ -1,31 +1,30 @@
 import {
   Route,
   createRoutesFromElements,
-  createBrowserRouter,
   RouterProvider,
   Navigate,
+  createHashRouter,
 } from "react-router-dom";
 
 import Layout from "./src/components/Layout";
-import NotFoundPage from "./src/views/NotFound.page";
 import AboutPage from "./src/views/About.page";
 import PhotographyPage from "./src/views/Photography.page";
 import DesignAndDevelopmentPage from "./src/views/DesignAndDevelopment.page";
 import PersonalProjectsPage from "./src/views/PersonalProjects.page";
 
-export default function Router() {
-  const router = createBrowserRouter(
+export default function BrowserRouter() {
+  const router = createHashRouter(
     createRoutesFromElements(
       <Route element={<Layout />}>
+        <Route path="/" element={<Navigate to="/about" replace={true} />} />
         <Route
-          path="/"
-          element={<Navigate to="/about" replace={true} />}
+          path="/design-and-dev"
+          element={<DesignAndDevelopmentPage />}
         />
-        <Route path="/design-and-dev" element={<DesignAndDevelopmentPage />} />
         <Route path="/personal-projects" element={<PersonalProjectsPage />} />
         <Route path="/photography" element={<PhotographyPage />} />
         <Route path="/about" element={<AboutPage />} />
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path="*" element={<Navigate to="/about" replace={true} />} />
       </Route>
     )
   );
